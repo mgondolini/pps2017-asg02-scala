571778116266409d009aab18b85c4293442b40e4
Monica Gondolini <monica.gondolini@studio.unibo.it>
2019-01-05 15:07:07 +0100

    fixed iml file.

--- src/u04lab/code/Complex.scala
+++ src/u04lab/code/Complex.scala
@@ -8,7 +8,7 @@ trait Complex {
 }
 
 object Complex {
-  def apply(re:Double, im:Double): Complex = ComplexImpl(re, im) // Fill here
+  def apply(re:Double, im:Double): Complex = new ComplexImpl(re, im) // Fill here
   private class ComplexImpl(override val re: Double, override val im: Double) extends Complex{
     override def +(c: Complex): Complex = Complex(re+c.re , im+c.im)
     override def *(c: Complex): Complex = {
